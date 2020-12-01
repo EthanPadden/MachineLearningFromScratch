cfb093de44b05b6ee74e9e2fb8abaf582aca8183
Ethan Padden <ethanpadden@gmail.com>
2020-12-01 13:57:19 +0000

    Add structure of get_candidate_thresholds

--- tools.py
+++ tools.py
@@ -14,3 +14,22 @@ def calculate_entropy(data, labels):
         sum += -1*label_proportion*(math.log(label_proportion, 2))
     print("Entropy = " + str(sum))
     return sum
+
+def get_candidate_thresholds(mappings, data, labels, attr_index):
+    pass
+    ### TODO
+    # # Sort the data and leb according to the attribute at the index
+    # print("Getting candidate thresholds for attribute at index " + str(attr_index))
+    # sorted(data, key=lambda x: x[1])
+    # data = mappings[0]
+    # data = data[data[:, attr_index].argsort()]
+
+    # Iterate through the rows:
+    #   If the classification changes:
+    #       Get the midpoint attribute value between this row and the last
+    #       Add it to the candidate list
+
+    # for row in data:
+
+
+    # Return the candidate list
