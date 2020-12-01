2e4fa21111414e7ae855f83046ca84b178e7009e
Ethan Padden <ethanpadden@gmail.com>
2020-12-01 15:52:31 +0000

    Add method to get candidate thresholds

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+
 def calculate_entropy(data, labels):
     sum = 0.0
     labels_unique = np.unique(labels, axis=0)
@@ -9,27 +10,38 @@ def calculate_entropy(data, labels):
     print(labels_unique)
     for label in labels_unique:
         label_count = len(labels[labels == label])
-        label_proportion = float(label_count)/float(len(labels))
+        label_proportion = float(label_count) / float(len(labels))
         print(label + " proportion = " + str(label_count) + "/" + str(len(labels)) + " = " + str(label_proportion))
-        sum += -1*label_proportion*(math.log(label_proportion, 2))
+        sum += -1 * label_proportion * (math.log(label_proportion, 2))
     print("Entropy = " + str(sum))
     return sum
 
-def get_candidate_thresholds(mappings, data, labels, attr_index):
-    pass
-    ### TODO
-    # # Sort the data and leb according to the attribute at the index
-    # print("Getting candidate thresholds for attribute at index " + str(attr_index))
-    # sorted(data, key=lambda x: x[1])
-    # data = mappings[0]
-    # data = data[data[:, attr_index].argsort()]
 
-    # Iterate through the rows:
-    #   If the classification changes:
-    #       Get the midpoint attribute value between this row and the last
-    #       Add it to the candidate list
+def get_candidate_thresholds(data, labels, attr_index):
+    # Make a copy of the data for sorting without affecting the original sort order
+    data_for_sorting = data.copy()
 
-    # for row in data:
+    # Sort the data according to the attribute at the index
+    data_for_sorting = data_for_sorting[data_for_sorting[:, attr_index].argsort()]
 
+    # Create a list to hold candidate thresholds
+    candidate_thresholds = []
+
+    for sorted_index in range(len(data_for_sorting) - 1):
+        current_unsorted_index = int(data_for_sorting[sorted_index][9])
+        next_unsorted_index = int(data_for_sorting[sorted_index + 1][9])
+        current_label_value = labels[current_unsorted_index]
+        next_label_value = labels[next_unsorted_index]
+
+        # Check if the classification changes
+        if (current_label_value != next_label_value):
+            # Get the midpoint attribute value between this row and the next
+            current_attr_value = data_for_sorting[sorted_index][attr_index]
+            next_attr_value = data_for_sorting[sorted_index + 1][attr_index]
+            midpoint = (current_attr_value + next_attr_value) / 2
+
+            # Add this to the list
+            candidate_thresholds.append(midpoint)
 
     # Return the candidate list
+    return candidate_thresholds
